0c9ef2f5d83b5f881c69824cb9e30764170c2222
Tattsum <kurohari35@gmail.com>
2017-06-20 10:04:10 +0900

    追加

---
+++ 3/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+  public static void main (String args[]){
+    System.out.println ("Hello,Java World");
+
+    System.out.print("初めての");
+    System.out.print("Javaプログラム");
+    System.out.println();
+
+    System.out.println("A long long time ago, "
+                       + "in a galaxy far away");
+  }
+}
