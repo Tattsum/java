9272bd80d8080375ab09d4032df9029e3f71ff14
Tattsum <kurohari35@gmail.com>
2017-06-20 10:06:21 +0900

    出力

---
+++ 3/PrintStrInt.java
@@ -0,0 +1,7 @@
+public class PrintStrInt {
+    public static void main(String args[]) {
+        System.out.println(57 + 32);
+        System.out.println("57 + 32 = " + 57 + 32);
+        System.out.println("57 + 32 = " + (57 + 32));
+    }
+}
