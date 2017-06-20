c6ec41c243dbc9baf4c117cad907999a08a1aee1
Tattsum <kurohari35@gmail.com>
2017-06-20 10:07:44 +0900

    変数の値の表示

---
+++ 3/PrintStrIntVar.java
@@ -0,0 +1,12 @@
+public class PrintStrIntVar {
+    public static void main(String args[]) {
+        int x = 12, y = 34;
+        int ans = x + y;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("ans = " + ans);
+        System.out.println("x + y = " + x + y);
+        System.out.println("(x + y) = " + (x + y));
+    }
+}
