43f8902ed7e39114333e2caeec63b9dec4575600
Tattsum <kurohari35@gmail.com>
2017-07-21 16:51:30 +0900

    インデント変更

--- 12/RainbowTurtleRectangle.java
+++ 12/RainbowTurtleRectangle.java
@@ -9,14 +9,14 @@ public class RainbowTurtleRectangle {
     private RainbowTurtle t;
 
     public RainbowTurtleRectangle(int xpos, int ypos, int speed, int width,int height) {
-	this.speed = speed;
-	this.width = width;
-	this.height = height;
-        this.movedLength = 0;
-	this.t = new RainbowTurtle();
-	this.t.move(xpos, ypos);
-	this.t.penDown();
-        this.co = 0;
+      	this.speed = speed;
+      	this.width = width;
+      	this.height = height;
+              this.movedLength = 0;
+      	this.t = new RainbowTurtle();
+      	this.t.move(xpos, ypos);
+      	this.t.penDown();
+              this.co = 0;
     }
 
     public void setColor(java.awt.Color c){
@@ -24,41 +24,40 @@ public class RainbowTurtleRectangle {
     }
 
     public void step() {
-	// speed分だけ進める
-	this.t.go(this.speed);
+      	// speed分だけ進める
+      	this.t.go(this.speed);
 
-	// これまで動いた長さにspeedを足す
-	this.movedLength += this.speed;
+      	// これまで動いた長さにspeedを足す
+      	this.movedLength += this.speed;
 
-	// 一辺の長さ(length)以上動いてたら，向きを変える
+      	// 一辺の長さ(length)以上動いてたら，向きを変える
 
-        if(this.co == 0){
-            if(this.movedLength>= this.width){
-                this.t.rotate(90);
-                this.movedLength = 0;
-                this.co = 1;
-            }
-        }
+              if(this.co == 0){
+                  if(this.movedLength>= this.width){
+                      this.t.rotate(90);
+                      this.movedLength = 0;
+                      this.co = 1;
+                  }
+              }
 
-        if(this.co == 1){
-            if(this.movedLength>= this.height){
-                this.t.rotate(90);
-                this.movedLength = 0;
-                this.co = 0;
-            }
-        }
-    }
-
-
+              if(this.co == 1){
+                  if(this.movedLength>= this.height){
+                      this.t.rotate(90);
+                      this.movedLength = 0;
+                      this.co = 0;
+                  }
+              }
+          }
 
 
-    public static void main(String[] args) {
 
-	RainbowTurtleRectangle rect1  = new RainbowTurtleRectangle(50,250,50,200,100);
 
-	while (true) {  // 実際に長方形を書く
-	    rect1.step();
-	}
-    }
+  public static void main(String[] args) {
 
+	   RainbowTurtleRectangle rect1  = new RainbowTurtleRectangle(50,250,50,200,100);
+
+	       while (true) {  // 実際に長方形を書く
+	          rect1.step();
+	       }
+  }
 }
