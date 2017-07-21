2aede2a6bd11dcd9892ce1f941ba31392b00735a
Tattsum <kurohari35@gmail.com>
2017-07-21 16:16:39 +0900

    正方形

---
+++ 12/AsciiSquare.java
@@ -0,0 +1,13 @@
+/**
+ * 正方形クラス
+ *  ここでは，正方形は幅と高さが等しい長方形と考えて，
+ *  長方形クラスのサブクラスとしている
+ */
+public class AsciiSquare extends AsciiRectangle {
+    public AsciiSquare(int edge) {
+	super(edge, edge); // 幅と高さが等しい長方形
+    }
+}
+
+
+ 
