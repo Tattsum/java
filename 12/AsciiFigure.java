4a591aa0ce615f4c9f0357bd4d1028b462a98495
Tattsum <kurohari35@gmail.com>
2017-07-21 16:21:12 +0900

    抽象クラス

--- 12/AsciiFigure.java
+++ 12/AsciiFigure.java
@@ -1,26 +1,24 @@
-public class AsciiFigure {
+abstract public class AsciiFigure {
     private String mark = "*"; // デフォルトは "*"
 
     /** mark を設定する */
     public void setMark(String mark) {
-	this.mark = mark;
+        this.mark = mark;
     }
 
     /** 1行に n個の mark を出力する
      *  サブクラス以外からは使われないと考えて，protectedにしている
      */
     protected void putLine(int n) {
-	for (int i = 1; i <= n; i++) {
-	    System.out.print(mark);
-	}
-	System.out.println();
+        for (int i = 1; i <= n; i++) {
+            System.out.print(mark);
+        }
+        System.out.println();
     }
 
-    /** 図形を描く．
-      * ただし，このクラスのdraw()は何も描かない．
-      * サブクラスで適切にオーバライドすること．
+    /**
+      *  図形を描く．
+      *  abstractメソッドなので，サブクラスで適切にオーバライドする．
       */
-    public void draw() {
-	/* do nothing */
-    }
+    abstract public void draw();
 }
